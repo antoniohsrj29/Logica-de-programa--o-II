79604428761355e9d49141cabd9cace09af5da78
Antonio Henrique <antoniohsrj@hotmail.com>
2023-07-20 11:50:33 -0300

    ASDASDAS

--- Calculadora/__init__.py
+++ Calculadora/__init__.py
@@ -4,37 +4,44 @@ from funcoes import Divisao
 from funcoes import Multiplicacao
 from funcoes import Exponencial
 
-def calcule(a,b,op):
+def calcule():
     resultado = 0
-    if type(a) == str or type(a) == bool:
-        print("O numero precisa ser um inteiro ou um float.")
-        resultado = "Não foi possivel obter um resultado."
-        return resultado
-    else:
-        pass
-    if type(b) == str or type(b) == bool:
-        print("O numero precisa ser um inteiro ou um float.")
-        resultado = "Não foi possivel obter um resultado."
-        return resultado
-    else:
-        pass
-    resultado = 0
-    if op not in ['+','-','/','*','**']:
+    a = input("Digite o primeiro número: ")
+    while a.isdigit() != True:
+        print("Preciso que digite um número.")
+        a = input("Digite novamente: ")    
+    a = int(a)
+    b = input("Digite o segundo número: ")
+    while b.isdigit() != True:
+        print("Preciso que digite um número.")
+        b = input("Digite novamente: ")    
+    b = int(b)
+    operacao = input("""Qual operação gostaria de ralizar?
+                     use:
+                        + para soma
+                        - para subtração
+                        / para divisão
+                        * para multiplica
+                        ** para exponenciação
+                    """)
+    if operacao not in ['+','-','/','*','**']:
         print("Operação não identificada!")
         resultado = "Não foi possivel realizar a operação."
-    elif op == '+':
+    elif operacao == '+':
         print(f"a soma é {Soma(a,b)}")
         resultado = Soma(a,b)
-    elif op == '-':
+    elif operacao == '-':
         print(f"a subtração é {Subtracao(a,b)}")
         resultado = Subtracao(a,b)
-    elif op == '/':
+    elif operacao == '/':
         print(f"a divisão é {Divisao(a,b)}")
         resultado = Divisao(a,b)
-    elif op == '*':
+    elif operacao == '*':
         print(f"a multiplicação é {Multiplicacao(a,b)}")
         resultado = Multiplicacao(a,b)
-    elif op == '**':
+    elif operacao == '**':
         print(f"a exponecial é {Exponencial(a,b)}")
         resultado = Exponencial(a,b)
     return resultado
+
+calcule()
